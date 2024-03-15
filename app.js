9304426e60f5debf20ad1482c9ebd5db472cb7cb
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-03-15 16:30:50 +0800

    create basic express app, with sequelize, MVC

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const db = require("./models/index");
+
+const testConnection = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+testConnection();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
